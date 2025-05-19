6d2e57bcfb6ec8ef1cd1791c9964af5025c9df6d
ZainSaeed115 <muhammadzainsaeed115@gmail.com>
2025-05-19 06:39:30 +0500

    modify axios file

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance=axios.create({
-    baseURL:'http://pos-backend-b5a5.onrender.com/api/v1',
+    baseURL:'https://pos-backend-b5a5.onrender.com/api/v1',
     // baseURL:'http://localhost:5676/api/v1',
     withCredentials:true
 })
